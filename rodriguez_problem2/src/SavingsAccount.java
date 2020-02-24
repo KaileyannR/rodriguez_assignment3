58c88381007c958d4e860df34b3482a81d0f70a0
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-02-23 22:07:04 -0500

    Update SavingsAccount.java

--- rodriguez_problem2/src/SavingsAccount.java
+++ rodriguez_problem2/src/SavingsAccount.java
@@ -1,1 +1,25 @@
+// Kaileyann Rodriguez
+// COP 3330
+// Programming Assignment 3
 
+import java.util.Scanner;
+public class SavingsAccount
+{
+	private static double annualInterestRate;
+	private static double savingsBalance;
+
+	public static void calculateMonthlyInterest()
+	{
+		savingsBalance = (savingsBalance * annualInterestRate) / 12;
+	}
+
+	public static void modifyInterestRate()
+	{
+		annualInterestRate = (savingsBalance * 12);
+	}
+
+	public static void main (String[] args)
+	{
+		// Savings Account Test
+	}
+}
