d0e30fdd99244f4eb79e5e113d49ea38994945e9
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-02-23 22:13:05 -0500

    Update SavingsAccountTest.java

--- rodriguez_problem2/src/SavingsAccountTest.java
+++ rodriguez_problem2/src/SavingsAccountTest.java
@@ -1,1 +1,22 @@
+// Kaileyann Rodriguez
+// COP 3330
+// Programming Assignment 3
+
+import java.util.Scanner;
+public class SavingsAccountTest
+{
+	public static void main (String args[])
+	{
+		Object saver1 = new Object();
+		Object saver2 = new Object();
+
+		saver1 = 2000.00;
+		saver2 = 3000.00;
+
+		double annualInterestRate = 0.04;
+		calculateMonthlyInterest();
+		
+		double annualInterestRate = 0.05;
+	}
+}
 
